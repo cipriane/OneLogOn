8e5c37011334c2e469cf73a03060e41e29d3c187
docallens <hartiganaaron@gmail.com>
2018-11-01 03:43:52 -0700

    Bring backend up a directory

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('server.urls')),
+    path('', include('backend.server.urls')),
 ]
