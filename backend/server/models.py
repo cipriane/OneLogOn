8469ff119d5fd9c1be91671117d971b5edf01422
ndubble <nicholasdubble@csus.edu>
2019-02-04 14:11:48 -0800

    added Visitors model to models.py

--- backend/server/models.py
+++ backend/server/models.py
@@ -6,3 +6,12 @@ class Student(models.Model):
     major = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Visitors(models.Model):
+    user_id = models.ForeignKey(SignIns, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, 
+        null=True)
+    last_name = models.CharField(max_length=30,
+        null=True)
+    isEmployee = models.BooleanField(default=False)
+    waiver = models.BooleanField(default=False)
