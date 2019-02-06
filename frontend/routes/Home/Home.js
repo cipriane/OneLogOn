840111fc0336dc9f6c1569ef0efd7b37de5f785e
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-02-06 14:39:42 -0800

    login page themed same as home page

--- frontend/routes/Home/Home.js
+++ frontend/routes/Home/Home.js
@@ -8,21 +8,33 @@ import Layout from 'common/Layout/Layout';
 import s from './Home.css';
 
 export default class Home extends Component {
-  handleClick = () => {
-    console.log('Button Clicked');
+  state = {
+    id: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+    console.log(this.state.id);
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+        id: evt.target.value
+    });
   }
 
   render() {
     return (
       <Layout>
-          <Form className={s.root}>
+          <Form className={s.root} onSubmit={this.handleSubmit}>
             <MainFormLayout>
-              <Form.Group controlId="formBasicEmail" >
+              <Form.Group >
                 <FormIcon url="https://proxy.duckduckgo.com/ip3/www.makerhq.org.ico" />
                 <Form.Label className={s.headerText}>Welcome to OneLogOn</Form.Label> <br/>
-                <Form.Control className={s.textfield} type="text" placeholder="Enter ID" />
+                <Form.Control className={s.textfield} type="text" placeholder="Enter ID" onChange={this.handleChange}/>
               </Form.Group>
-              <FancyButton label="Log in" onClick={this.handleClick}/>
+              <FancyButton label="Log in" type="submit"/>
             </MainFormLayout>
         </Form>
       </Layout>
