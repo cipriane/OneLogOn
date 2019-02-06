1954c1a54533254172b4261750627d15c2e5cdfb
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-02-06 12:52:07 -0800

    home page components extracted

---
+++ frontend/common/MainFormLayout/MainFormLayout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import s from './MainFormLayout.css';
+
+export default class MainFormLayout extends Component {
+  render() {
+    return (
+      <div className={s.root}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- frontend/routes/Home/Home.js
+++ frontend/routes/Home/Home.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
+import MainFormLayout from 'common/MainFormLayout/MainFormLayout';
+import FancyButton from 'common/FancyButton/FancyButton';
+import FormIcon from 'common/FormIcon/FormIcon';
 import Layout from 'common/Layout/Layout';
 import s from './Home.css';
 
 export default class Home extends Component {
+  handleClick = () => {
+    console.log('Button Clicked');
+  }
+
   render() {
     return (
       <Layout>
           <Form className={s.root}>
-            <div className={s.formContent}>
+            <MainFormLayout>
               <Form.Group controlId="formBasicEmail" >
-              <div>
-                <img src="https://proxy.duckduckgo.com/ip3/www.makerhq.org.ico" className={s.icon} alt="User Icon" />
-              </div>
-              <Form.Label className={s.headerText}>Welcome to OneLogOn</Form.Label> <br/>
-              <Form.Control type="text" placeholder="Enter ID" />
+                <FormIcon url="https://proxy.duckduckgo.com/ip3/www.makerhq.org.ico" />
+                <Form.Label className={s.headerText}>Welcome to OneLogOn</Form.Label> <br/>
+                <Form.Control className={s.textfield} type="text" placeholder="Enter ID" />
               </Form.Group>
-              <input type="submit" value="Log In"></input>
-            </div>
+              <FancyButton label="Log in" onClick={this.handleClick}/>
+            </MainFormLayout>
         </Form>
       </Layout>
     );
