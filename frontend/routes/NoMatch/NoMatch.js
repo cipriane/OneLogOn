e1c4a7e0704346547c2c5ed4934ff6ca479e7711
Serge <skarchmit@gmail.com>
2019-02-10 18:01:07 -0800

    updated route to look like everyting else

--- frontend/routes/NoMatch/NoMatch.js
+++ frontend/routes/NoMatch/NoMatch.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import s from './NoMatch.css';
+import Layout from 'common/Layout/Layout';
+import FullScreenLayout from 'common/FullScreenLayout/FullScreenLayout';
+import Form from 'react-bootstrap/Form';
+import MainFormLayout from 'common/MainFormLayout/MainFormLayout';
+import FancyButton from 'common/FancyButton/FancyButton';
+
+
 
 export default class NoMatch extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Page Not Found</h1>
-        <Link to='/'>Go To Home</Link>
-      </div>
-    );
-  }
+
+	handleSubmit = async (event) => {
+		this.props.history.push('/');
+	};
+
+
+	render() {
+		return (
+			<Layout>
+				<FullScreenLayout>
+					<Form onSubmit={this.handleSubmit}>
+						<MainFormLayout>
+							<Form.Group >
+								<h1 className={s.test}>
+									Page Not Found
+								</h1>
+								<FancyButton 
+									label="Go Home"
+									type = "submit"
+								/>
+							</Form.Group >
+						</MainFormLayout>
+					</Form>
+				</FullScreenLayout>
+			</Layout>
+		);
+	}
 }
