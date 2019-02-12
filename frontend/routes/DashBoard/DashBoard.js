32983b1f67547d86d84f8c55cadb871e625254bc
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-02-12 00:21:48 -0800

    dashboard static template made more responsive

--- frontend/routes/DashBoard/DashBoard.js
+++ frontend/routes/DashBoard/DashBoard.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
-import DashBoardNav from 'common/DashBoardNav/DashBoardNav';
-import DashBoardView from 'common/DashBoardView/DashBoardView';
-import DashBoardHeader from 'common/DashBoardHeader/DashBoardHeader';
+import { Button } from 'react-bootstrap';
+import dashboardRoutes from 'routes/routes';
 import s from './DashBoard.css';
 
 export default class DashBoard extends Component {
+  state = {
+    mainContent: dashboardRoutes[0].component,
+    sideContent: null,
+    currentPageId: 0
+  };
+
+  openSideMenu = () => {
+    document.getElementById('sideMenu').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+  }
+
+  closeSideMenu = () => {
+    document.getElementById('sideMenu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+
   render() {
     return (
-      <div>
-        <DashBoardHeader />
-        <DashBoardNav />
-        <DashBoardView />
+      <div className={s.root}>
+        <nav className={s.navbar}>
+          <span>
+            <Button className={s.burger} variant="outline-secondary" onClick={this.openSideMenu}>
+              <svg width="30" height="30">
+                  <path d="M0,5 30,5" stroke="#fff" strokeWidth="5"/>
+                  <path d="M0,14 30,14" stroke="#fff" strokeWidth="5"/>
+                  <path d="M0,23 30,23" stroke="#fff" strokeWidth="5"/>
+              </svg>
+            </Button>
+          </span>
+
+          <div className={s.navbarNav}>
+            <p className={s.detail}>Signed in as: <span className={s.name}>Admin</span> </p>
+          </div>
+        </nav>
+
+        <div id="sideMenu" className={s.sideNav}>
+          <a href="#" className={s.btnClose} onClick={this.closeSideMenu}>&times;</a>
+          {dashboardRoutes.map((route) => {
+            return <a key={route.id} href={route.path}>{route.name}</a>
+          })}
+        </div>
+
+        <div id="main" className={s.main}>
+          {< this.state.mainContent />}
+        </div>
       </div>
     )
   }
