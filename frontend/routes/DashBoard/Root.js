5726073d2846bfb52513434506b45507059dc2c6
docallens <hartiganaaron@gmail.com>
2019-02-18 01:08:01 -0800

    Basic check-in pages.  WIP

--- frontend/routes/DashBoard/Root.js
+++ frontend/routes/DashBoard/Root.js
@@ -4,6 +4,7 @@ import Dashboard from 'routes/DashBoard/DashBoard';
 import Settings from 'routes/Settings/Settings';
 import ManageUsers from 'routes/ManageUsers/ManageUsers';
 import Statistics from 'routes/Statistics/Statistics';
+import Kiosk from 'routes/Kiosk/Kiosk';
 import NoMatch from 'routes/NoMatch/NoMatch';
 
 const Root = ({ match }) => (
@@ -13,6 +14,7 @@ const Root = ({ match }) => (
       <Route path={`${match.url}/statistics`} component={Statistics} />
       <Route path={`${match.url}/manage`} component={ManageUsers} />
       <Route path={`${match.url}/settings`} component={Settings} />
+      <Route path={`${match.url}/kiosk`} component={Kiosk} />
       <Route component={NoMatch} />
     </Switch>
   </Dashboard>
