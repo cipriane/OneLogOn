829be0c5c22bc28b86ee5a7d1166e22dff62663f
Serge <skarchmit@gmail.com>
2019-02-19 00:20:54 -0800

    added options for requried and autoFocus

--- frontend/common/FancyTextField/FancyTextField.js
+++ frontend/common/FancyTextField/FancyTextField.js
@@ -3,10 +3,15 @@ import s from './FancyTextField.css';
 import Form from 'react-bootstrap/Form';
 //import Control from 'react-bootstrap/Form/From';
 
-export default class Textfield extends React.Component {
+export default class FancyTextField extends React.Component {
   render() {
+    const _required = this.props.required == null ? false : true;
+    const _autofocus = this.props.autoFocus == null ? false : true;
+
     return (
       <Form.Control
+        required={_required}
+        autoFocus={_autofocus}
         className={s.textfield}
         type={this.props.type}
         placeholder={this.props.placeholder}
