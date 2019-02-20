b422b3780272f7cc3adf4edaced0fbff2b5544fc
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-02-19 19:18:41 -0800

    more assets added, temp new colors

--- frontend/common/FancyFormHeader/FancyFormHeader.js
+++ frontend/common/FancyFormHeader/FancyFormHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import s from './FancyFormHeader.css';
 import Form from 'react-bootstrap/Form';
-import logo from 'assets/logo-full.png';
+import logo from 'assets/logo-full-color.png';
 import FormIcon from 'common/FormIcon/FormIcon';
 
 export default class FancyFormHeader extends React.Component {
