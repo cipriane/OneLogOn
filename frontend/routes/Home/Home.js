2abe5be6f9d1d106163b8c89166e6d77b7370571
Serge <skarchmit@gmail.com>
2019-02-21 20:33:21 -0800

    landing page init

--- frontend/routes/Home/Home.js
+++ frontend/routes/Home/Home.js
@@ -7,52 +7,74 @@ import FancyButton from 'common/FancyButton/FancyButton';
 import FancyTextField from 'common/FancyTextField/FancyTextField';
 import FancyFormHeader from 'common/FancyFormHeader/FancyFormHeader';
 import Layout from 'common/Layout/Layout';
+import { Container, Jumbotron, Row, Col } from 'react-bootstrap';
 import s from './Home.css';
+//import Jumbotron from 'react-bootstrap/Jumbotron'
+//import Container from 'react-bootstrap/Container'
 
 export default class Home extends Component {
-  constructor(...args) {
-    super(...args);
-
-    this.state = { validated: false, id: '' };
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.setState({ validated: true });
+  handleLogInButton = event => {
+    this.props.history.push('/login');
   };
 
-  handleChange = evt => {
-    this.setState({
-      id: evt.target.value,
-    });
+  handleRegisterButton = event => {
+    this.props.history.push('/register');
   };
 
   render() {
-    const { validated } = this.state;
+    const buttonTextLogin = 'Log In';
+    const buttonTextRegister = 'Register';
+
     return (
-      <Layout>
-        <FullScreenLayout>
-          <Form noValidate validated={validated} onSubmit={this.handleSubmit}>
-            <MainFormLayout>
-              <Form.Group>
-                <FancyFormHeader text="Welcome to OneLogOn" />
-                <FancyTextField
-                  autoFocus
-                  required
-                  type="text"
-                  placeholder="Enter ID"
-                  onChange={this.handleChange}
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                <Form.Control.Feedback type="invalid">
-                  Please enter a valid ID.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <FancyButton label="Check in" type="submit" />
-            </MainFormLayout>
-          </Form>
-        </FullScreenLayout>
-      </Layout>
+      <Container className={s.background}>
+        <Row>
+          <Col>
+            <h1 className={`${s.title} ${s.centered}`}>OneLogOn</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h3 className={`${s.tagline} ${s.centered}`}>
+              A simple Open Source visitor check-in and statistics aggregation system for your
+              facility or site.
+            </h3>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className={s.fiftyfifty}>
+              <FancyButton
+                label={buttonTextRegister}
+                onClick={this.handleRegisterButton}
+                type="submit"
+              />
+            </div>
+            <div className={s.fiftyfifty}>
+              <FancyButton
+                label={buttonTextLogin}
+                onClick={this.handleLogInButton}
+                type="submit"
+                className={s.secondaryButton}
+              />
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className={s.functionality}>
+            <h1 className={s.centered}>Functionality</h1>
+            <p>
+              /* TO DO */
+              <br />
+              Add some diagrams / screenshots of the awesome app
+              <br />
+              Carousel?
+            </p>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
