afbd2a15716b4708bab15a3bb736373158be07a0
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-02-23 18:53:18 -0800

    simple settings page added

--- frontend/routes/DashBoard/Manage/Manage.js
+++ frontend/routes/DashBoard/Manage/Manage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SimpleHeader from 'common/SimpleHeader/SimpleHeader';
 
 export default class ManageUsers extends Component {
   render() {
     return (
       <div>
-        <h1>Manage</h1>
+        <SimpleHeader title="Manage" />
       </div>
     );
   }
--- frontend/routes/DashBoard/Settings/Settings.js
+++ frontend/routes/DashBoard/Settings/Settings.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import SimpleHeader from 'common/SimpleHeader/SimpleHeader';
+import FancyButton from 'common/FancyButton/FancyButton';
+import { Form, Button } from 'react-bootstrap';
+import s from './Settings.css';
 
 export default class Settings extends Component {
+  state = {
+    welcomeMessage:
+      'A simple Open Source visitor check-in and statistics aggregation system for your facility or site.',
+  };
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div>
-        <h1>Settings</h1>
+        <SimpleHeader title="Settings" />
+        <Form className={s.form} onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBasicMessage">
+            <Form.Label>Checkin Message:</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.welcomeMessage}
+              name="welcomeMessage"
+              onChange={this.handleChange}
+            />
+            <Form.Text className="text-muted">Edit the welcome message on the home page.</Form.Text>
+          </Form.Group>
+
+          <div className={s.buttonBar}>
+            <Button type="submit" variant="success" label="Save">
+              Save
+            </Button>
+          </div>
+        </Form>
       </div>
     );
   }
