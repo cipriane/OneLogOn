a07a08f190a217544dfd3cc02dfc4d3d82215e83
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-02-23 19:01:59 -0800

    username and password fields added

--- frontend/routes/DashBoard/Settings/Settings.js
+++ frontend/routes/DashBoard/Settings/Settings.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SimpleHeader from 'common/SimpleHeader/SimpleHeader';
-import FancyButton from 'common/FancyButton/FancyButton';
 import { Form, Button } from 'react-bootstrap';
 import s from './Settings.css';
 
@@ -8,6 +7,8 @@ export default class Settings extends Component {
   state = {
     welcomeMessage:
       'A simple Open Source visitor check-in and statistics aggregation system for your facility or site.',
+    username: 'Admin',
+    password: 'password',
   };
 
   handleChange = event => {
@@ -29,21 +30,41 @@ export default class Settings extends Component {
         <SimpleHeader title="Settings" />
         <Form className={s.form} onSubmit={this.handleSubmit}>
           <Form.Group controlId="formBasicMessage">
-            <Form.Label>Checkin Message:</Form.Label>
+            <Form.Label className={s.label}>Checkin Message:</Form.Label>
             <Form.Control
               type="text"
-              value={this.state.welcomeMessage}
               name="welcomeMessage"
+              value={this.state.welcomeMessage}
               onChange={this.handleChange}
             />
             <Form.Text className="text-muted">Edit the welcome message on the home page.</Form.Text>
           </Form.Group>
 
-          <div className={s.buttonBar}>
-            <Button type="submit" variant="success" label="Save">
-              Save
-            </Button>
-          </div>
+          <Form.Group controlId="formBasicMessage">
+            <Form.Label className={s.label}>Username:</Form.Label>
+            <Form.Control
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <Form.Text className="text-muted">Edit your username.</Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicMessage">
+            <Form.Label className={s.label}>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value="&#183;&#183;&#183;&#183;&#183;&#183;&#183;&#183;&#183;"
+              onChange={this.handleChange}
+            />
+            <Form.Text className="text-muted">Edit your password.</Form.Text>
+          </Form.Group>
+
+          <Button type="submit" variant="success" label="Save">
+            Save
+          </Button>
         </Form>
       </div>
     );
