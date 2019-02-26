fac85ed86141f101098aebc851878fba2702c564
docallens <hartiganaaron@gmail.com>
2019-02-26 01:11:17 -0800

    Add custom fields to JWT

---
+++ backend/server/custom_jwt.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.username
+        token['email'] = user.email
+        token['is_staff'] = user.is_staff
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
