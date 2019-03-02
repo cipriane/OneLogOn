fb8f96fee710a3176d59ca32b1efd67b699aeac6
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-03-02 15:47:55 -0800

    all settings components seperated

--- frontend/routes/DashBoard/Settings/Settings.js
+++ frontend/routes/DashBoard/Settings/Settings.js
@@ -1,85 +1,23 @@
 import React, { Component } from 'react';
 import SimpleHeader from 'common/SimpleHeader/SimpleHeader';
-import { Form, Button, Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import WelcomeMessage from 'common/WelcomeMessage/WelcomeMessage';
+import Profile from 'common/Profile/Profile';
 import ReasonList from 'common/Reasons/ReasonList/ReasonList';
 import s from './Settings.css';
 
 export default class Settings extends Component {
-  state = {
-    welcomeMessage:
-      'A simple Open Source visitor check-in and statistics aggregation system for your facility or site.',
-    username: 'Admin',
-    password: 'password',
-    isLoading: false,
-    error: null,
-  };
-
-  handleChange = event => {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log('submitted');
-  };
-
   render() {
     return (
       <div>
         <SimpleHeader title="Settings" />
 
         <Container className={s.padding}>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId="formBasicMessage">
-              <Form.Label className={s.label}>Checkin Message:</Form.Label>
-              <Form.Control
-                type="text"
-                name="welcomeMessage"
-                value={this.state.welcomeMessage}
-                onChange={this.handleChange}
-              />
-              <Form.Text className="text-muted">
-                Edit the welcome message on the home page.
-              </Form.Text>
-            </Form.Group>
-            <Button type="submit" variant="outline-success" label="Save" className={s.submit}>
-              Save changes
-            </Button>
-          </Form>
+          <WelcomeMessage />
         </Container>
 
         <Container className={s.padding}>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId="formBasicUsername">
-              <Form.Label className={s.label}>Username:</Form.Label>
-              <Form.Control
-                type="text"
-                name="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-              <Form.Text className="text-muted">Edit your username.</Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label className={s.label}>Password:</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                value="&#183;&#183;&#183;&#183;&#183;&#183;&#183;&#183;&#183;"
-                onChange={this.handleChange}
-              />
-              <Form.Text className="text-muted">Edit your password.</Form.Text>
-            </Form.Group>
-            <Button type="submit" variant="outline-success" className={s.submit}>
-              Save changes
-            </Button>
-          </Form>
+          <Profile />
         </Container>
 
         <Container className={s.padding}>
