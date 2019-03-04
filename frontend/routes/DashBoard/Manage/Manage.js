42515c3a070936b515a93506951b2bf0b07bf129
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-03-03 17:50:30 -0800

    edit user button shows custom visitor dialog for future editing

--- frontend/routes/DashBoard/Manage/Manage.js
+++ frontend/routes/DashBoard/Manage/Manage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import SimpleHeader from 'common/SimpleHeader/SimpleHeader';
-import { Container, Badge, Button, Table } from 'react-bootstrap';
+import { Container, Badge, Button, Table, Modal, Form, InputGroup } from 'react-bootstrap';
 import myFetch from 'utils/fetch';
 import s from './Manage.css';
 
@@ -9,6 +9,8 @@ export default class ManageUsers extends Component {
     visitors: [],
     isLoading: false,
     error: false,
+    modalShow: false,
+    selectedVisitor: {},
   };
 
   async componentDidMount() {
@@ -23,7 +25,6 @@ export default class ManageUsers extends Component {
         visitors: data,
         isLoading: false,
       });
-      console.log(this.state.visitors);
     } catch (err) {
       this.setState({
         isLoading: false,
@@ -32,11 +33,25 @@ export default class ManageUsers extends Component {
     }
   }
 
+  modalClose = () => {
+    this.setState({ modalShow: false });
+  };
+
+  modalOpen = visitor => {
+    this.setState({ selectedVisitor: visitor, modalShow: true });
+  };
+
   render() {
     const { visitors } = this.state;
     return (
       <div>
         <SimpleHeader title="Manage" />
+
+        <MyVerticallyCenteredModal
+          show={this.state.modalShow}
+          onHide={this.modalClose}
+          visitor={this.state.selectedVisitor}
+        />
 
         <Container fluid>
           <Table striped bordered hover size="sm">
@@ -77,7 +92,12 @@ export default class ManageUsers extends Component {
                     )}
                   </td>
                   <td>
-                    <Button className={s.button} variant="info" size="sm">
+                    <Button
+                      className={s.button}
+                      variant="info"
+                      size="sm"
+                      onClick={() => this.modalOpen(visitor)}
+                    >
                       Edit User
                     </Button>
                     <Button className={s.button} variant="danger" size="sm">
@@ -90,6 +110,95 @@ export default class ManageUsers extends Component {
           </Table>
         </Container>
       </div>
+    );
+  }
+}
+
+class MyVerticallyCenteredModal extends Component {
+  state = {
+    first_name: '',
+    last_name: '',
+    waiver_signed: false,
+    is_employee: false,
+  };
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = () => {};
+
+  handleSave = () => {};
+
+  render() {
+    const { visitor } = this.props;
+    return (
+      <Modal {...this.props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {visitor.first_name + ' ' + visitor.last_name + ' (' + visitor.visitor_id + ')'}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>First Name</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="text"
+              name="first_name"
+              placeholder={visitor.first_name}
+              value={this.state.first_name}
+              onChange={this.handleChange}
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Last Name</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="text"
+              name="last_name"
+              placeholder={visitor.last_name}
+              value={this.state.last_name}
+              onChange={this.handleChange}
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Waiver</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Button
+              onClick={() => this.setState({ waiver_signed: !this.state.waiver_signed })}
+              variant={this.state.waiver_signed ? 'primary' : 'danger'}
+            >
+              {this.state.waiver_signed ? 'Waiver Signed' : 'Waiver Not Signed'}
+            </Button>
+            <InputGroup.Prepend>
+              <InputGroup.Text>Role</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Button
+              onClick={() => this.setState({ is_employee: !this.state.is_employee })}
+              variant={this.state.is_employee ? 'primary' : 'success'}
+            >
+              {this.state.is_employee ? 'Employee' : 'Visitor'}
+            </Button>
+          </InputGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={this.props.onHide}>
+            Save Changes
+          </Button>
+          <Button variant="white" onClick={this.props.onHide}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
