6f66c4a573a9471bd878e1f9626f0e776b00b647
ndubble <nicholasdubble@csus.edu>
2019-03-11 19:15:13 -0700

    bleh

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -12,21 +12,11 @@ urlpatterns = [
     path('api/companies/create', views.CompanyCreateView.as_view()),
     path('api/companies/<pk>', views.CompanyDetailView.as_view()),
     path('api/companies/message', views.CompanyMessageView.as_view()),
-
     path('api/visitors', views.VisitorsListView.as_view()),
-<<<<<<< HEAD
-    path('api/visitors/create/', views.VisitorsCreateView.as_view()),
-    path('api/visitors/<pk>', views.VisitorsDetailView.as_view()),
-    path('api/visitors/<pk>/waiver/', views.VisitorsUpdateWaiverView.as_view()),
-    path('api/listreasons', views.ListReasonsListView.as_view()),
-    path('api/listreasons/create/', views.ListReasonsCreateView.as_view()),
-    path('api/listreasons/<pk>', views.ListReasonsDetailView.as_view()),
-=======
     path('api/visitors/create', views.VisitorsCreateView.as_view()),
     path('api/visitors/<visitor_id>', views.VisitorsDetailView.as_view()),
     path('api/visitors/<pk>/update', views.VisitorsUpdateView.as_view()),
-
->>>>>>> upstream/master
+    path('api/visitors/<pk>/waiver/', views.VisitorsUpdateWaiverView.as_view()),
     path('api/checkins', views.CheckInsListView.as_view()),
     path('api/checkins/create', views.CheckInsCreateView.as_view()),
     path('api/checkins/<pk>', views.CheckInsDetailView.as_view()),
