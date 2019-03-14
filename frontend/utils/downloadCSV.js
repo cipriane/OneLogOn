e760dda4680b77de19e27d59190314914d705f40
docallens <hartiganaaron@gmail.com>
2019-03-13 20:32:29 -0700

    Export modal now functioning

--- frontend/utils/downloadCSV.js
+++ frontend/utils/downloadCSV.js
@@ -1,10 +1,10 @@
-function downloadCSV(data) {
+function downloadCSV(data, filename = 'report') {
   const blob = new Blob([data], { type: 'text/csv' });
   const url = window.URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.setAttribute('hidden', '');
   a.setAttribute('href', url);
-  a.setAttribute('download', 'download.csv');
+  a.setAttribute('download', `${filename}.csv`);
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
