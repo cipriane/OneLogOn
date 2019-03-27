dc702ef3b521fb1c8618511ad7a9b1fd7c4f59d8
docallens <hartiganaaron@gmail.com>
2019-03-26 19:56:01 -0700

    Remove debug statements

--- backend/server/views.py
+++ backend/server/views.py
@@ -70,10 +70,8 @@ class CompanyMessageView(APIView):
     Handle company check-in messages
     """
     permission_classes = (IsAuthenticated,)
-    print("Got here 2")
 
     def post(self, request, format='json'):
-        print("Got here 3")
         """
         POST /api/companies/message
         Set new company message
@@ -105,7 +103,6 @@ class CompanyMessageView(APIView):
         Returns company message
         Required Parameters: None
         """
-        print("Got here 1")
         try:
             id = request.user.id
             try:
