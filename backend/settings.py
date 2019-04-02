d9eb3279e9585b288d06f8f0382502f35d6a201d
Serge <skarchmit@gmail.com>
2019-04-01 17:55:42 -0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from datetime import timedelta
 
+from backend import secret
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
