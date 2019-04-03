97e065c7e82fe7f19fae99f535c5eb74f1532edd
docallens <hartiganaaron@gmail.com>
2019-04-03 15:03:44 -0700

    Final push

--- backend/settings_secret.py
+++ backend/settings_secret.py
@@ -1,9 +1,9 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i@t_*n#ee)*smb_@utqo=pk(v7ubqb1)0=)*hy6-^jt5dq@l@!'
 
-EMAIL_HOST = 'HOST'
-EMAIL_HOST_USER = 'HOST USER'
-EMAIL_HOST_PASSWORD = 'PASSWORD'
-EMAIL_PORT = 0
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'oneBitOff'
+EMAIL_HOST_PASSWORD = 'yiq4ZPhgEsDVfDZvPHNKVHyE'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
