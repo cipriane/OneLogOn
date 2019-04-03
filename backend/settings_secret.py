857a3591414d81304376caa57e7cb7b11dd8d072
Aaron <AaronHartigan@users.noreply.github.com>
2019-04-03 14:58:48 -0700

    Delete settings_secret.py

--- backend/settings_secret.py
+++
@@ -1,9 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i@t_*n#ee)*smb_@utqo=pk(v7ubqb1)0=)*hy6-^jt5dq@l@!'
-
-EMAIL_HOST = 'HOST'
-EMAIL_HOST_USER = 'HOST USER'
-EMAIL_HOST_PASSWORD = 'PASSWORD'
-EMAIL_PORT = 0
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
